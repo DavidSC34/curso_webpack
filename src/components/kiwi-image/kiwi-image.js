9690699d6ebe0f3956f34f56d4bcf75bde543c32
DavidSC <david@wbcboxing.com>
2019-12-06 12:47:57 -0600

    Leccion 6-2 Crear un Kiwi Component, agregar js,css y crear Multipage App

---
+++ src/components/kiwi-image/kiwi-image.js
@@ -0,0 +1,17 @@
+import Kiwi form './Kiwi.jpg';
+import './kiwi-image.css';
+
+class KiwiImage{
+
+  render(){
+     const img = document.createElement('img');
+     img.src = Kiwi;
+     img.alt = 'Kiwi';
+     img.classLsit.add('kiwi-image');
+
+     const bodyElement = document.querySelector('body');
+     bodyElement.appenChild(img);
+  }
+}
+
+export defualt KiwiImage;
